9d8a5e5b7a3df10d32084f21264d8952623516d9
Julio Picazo <julio.picazo@softtek.com>
2021-09-21 17:02:06 -0500

    Second commit now

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Julio Cesar Picazo Guevara
+          JulioSistems
         </a>
       </header>
     </div>
